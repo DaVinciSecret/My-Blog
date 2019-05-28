dcd988bceac9e7f22f8d75714f05b731a39324de
Diva <37040217+DaVinciSecret@users.noreply.github.com>
2019-05-28 22:32:38 +0800

    ajax封装，并promise化

---
+++ ajax_promise.js
@@ -0,0 +1,61 @@
+function ajax({url,type,data,dataType}){
+  return new Promise(function(open,err){
+		//1. 创建xhr对象
+		var xhr=new XMLHttpRequest();
+		//2.绑定监听事件
+		xhr.onreadystatechange=function(){
+			if(xhr.readyState==4&&xhr.status==200){
+				if(dataType!==undefined
+					&&dataType.toLowerCase()==="json")
+					var res=JSON.parse(xhr.responseText)
+				else
+					var res=xhr.responseText
+					
+				open(res);
+			}
+		}
+		if(type.toLowerCase()=="get"&&data!=undefined){
+			url+="?"+data;
+		}
+		//3.打开连接
+		xhr.open(type,url,true);
+		if(type.toLowerCase()==="post")
+			//增加：设置请求消息头
+			xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+		//4.发送请求
+		if(type.toLowerCase()=="post"&&data!==undefined)
+			xhr.send(data);
+		else
+			xhr.send(null);
+  })
+}
+
+ Promise.all([
+        ajax('http://127.0.0.1:8080/liang',"get",(res)=>{
+            console.log(res);
+        }),
+        ajax('http://127.0.0.1:8080/yan',"get",(res)=>{
+            console.log(res);
+        }),
+        ajax('http://127.0.0.1:8080/dong',"get",(res)=>{
+            console.log(res);
+        })
+    ]).then(()=>{
+        console.log("sss");
+    }).catch((err)=>{
+        console.error(err);
+    })
+
+//TODO:
+(async function(){
+	//ES7
+	var res=await ajax({
+		url:"http://localhost:3000/index/",
+		type:"get",
+		dataType:"json"
+	})
+	//ES6
+	.then(res=>{
+		... ...
+	})
+})();
